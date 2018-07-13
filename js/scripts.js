1d11a20d01f6214f23a320e196adea8f5eff3ca1
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-07-13 12:15:21 +0300

    add jQuery user interface logic

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $("form#ping-pong").submit(function(event){
+    event.preventDefault();
+    var number = parseInt($("input#number").val());
+    var result = pingPong(number);
+    $("#result").text(result);
+  });
+});
+
+var pingPong = function(number) {
+  if ((number % 3 === 0) && (number % 5 === 0)) {
+    return "pingpong";
+}
+  else if ((number % 3 === 0)){
+    return "ping";
+}
+  else if ((number % 5 === 0)){
+    return "pong";
+  }
+  else {
+    return "nil";
+  }
+};
